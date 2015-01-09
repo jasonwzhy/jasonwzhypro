da1e12eae018f90123685cd5ae4957f03ca463a2
jasonwzhy <jasonwzhy@gmail.com>
2015-01-10 03:17:37 +0800

    add wechat check

--- wechat/views.py
+++ wechat/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import hashlib
 def index(request):
-	return HttpResponse("hello wechat")
+	if request.method == 'GET':
+		signature = request.GET.get('signature','')
+		timestamp = request.GET.get('timestamp','')
+		nonce = request.GET.get('nonce','')
+		echostr=request.GET.get('echostr','')
+		token="jasonwang"
+		hslst = [token,timestamp,nonce]
+		hslst.sort()
+		hstr = "%s%s%s"%tuple(hslst)
+		hstr = hashlib.sha1(hstr).hexdigest()
+		if hstr==signature:
+			return HttpResponse(signature)
+		else:
+			return HttpResponse(None)
 # Create your views here.
