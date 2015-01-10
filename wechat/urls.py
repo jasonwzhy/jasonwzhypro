a0564f308e0e92c9927432b6e6092d98d85c3bd8
jasonwzhy <jasonwzhy@gmail.com>
2015-01-11 01:22:20 +0800

    add wechat_sdk

--- wechat/urls.py
+++ wechat/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import patterns, include, url
 from wechat import views
 urlpatterns = patterns('',
 	url(r'^$',views.index),
-	url(r'^ifsverify/$',views.ifsverify),
 	url(r'^manage/',views.manageindex),
 )
--- wechat/views.py
+++ wechat/views.py
@@ -1,19 +1,25 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 import hashlib
 from django.views.decorators.csrf import csrf_exempt
 from xml.etree import ElementTree as etree
 
+from wechat_sdk.basic import WechatBasic
+from wechat_sdk.messages import (
+    TextMessage, VoiceMessage, ImageMessage, VideoMessage, LinkMessage, LocationMessage, EventMessage
+)
+token = "jasonwang"
 # Create your views here.
 @csrf_exempt
-def ifsverify(request):
+def index(request):
 	if request.method == 'GET':
 		print request.GET
 		signature = request.GET.get('signature','')
 		timestamp = request.GET.get('timestamp','')
 		nonce = request.GET.get('nonce','')
 		echostr=request.GET.get('echostr','')
-		token="jasonwang"
+		
 		hslst = [token,timestamp,nonce]
 		hslst.sort()
 		hstr = "%s%s%s"%tuple(hslst)
@@ -21,13 +27,48 @@ def ifsverify(request):
 		print hstr,signature
 		if hstr==signature:
 			return HttpResponse(echostr)
-			#return True
 		else:
-			#return HttpResponse(None)
-			return False
+			return HttpResponse(None)
 	elif request.method == 'POST':
-		print request.body
-def index(request):
-	return HttpResponse('hello wechat')
+		signature = request.GET.get('signature','')
+		timestamp = request.GET.get('timestamp','')
+		nonce = request.GET.get('nonce','')
+		body_text = request.body
+		
+		wechat = WechatBasic(token=token)
+		if wechat.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
+			wechat.parse_data(body_text)
+			message = wechat.get_message()
+			if isinstance(message, TextMessage):
+				response = wechat.response_text(content=u'文字信息')
+		elif isinstance(message, VoiceMessage):
+			response = wechat.response_text(content=u'语音信息')
+		elif isinstance(message, ImageMessage):
+			response = wechat.response_text(content=u'图片信息')
+		elif isinstance(message, VideoMessage):
+			response = wechat.response_text(content=u'视频信息')
+		elif isinstance(message, LinkMessage):
+			response = wechat.response_text(content=u'链接信息')
+		elif isinstance(message, LocationMessage):
+			response = wechat.response_text(content=u'地理位置信息')
+		elif isinstance(message, EventMessage):  # 事件信息
+			if message.type == 'subscribe':  # 关注事件(包括普通关注事件和扫描二维码造成的关注事件)
+				if message.key and message.ticket:  # 如果 key 和 ticket 均不为空，则是扫描二维码造成的关注事件
+					response = wechat.response_text(content=u'用户尚未关注时的二维码扫描关注事件')
+				else:
+					response = wechat.response_text(content=u'普通关注事件')
+			elif message.type == 'unsubscribe':
+				response = wechat.response_text(content=u'取消关注事件')
+			elif message.type == 'scan':
+				response = wechat.response_text(content=u'用户已关注时的二维码扫描事件')
+			elif message.type == 'location':
+				response = wechat.response_text(content=u'上报地理位置事件')
+			elif message.type == 'click':
+				response = wechat.response_text(content=u'自定义菜单点击事件')
+			elif message.type == 'view':
+				response = wechat.response_text(content=u'自定义菜单跳转链接事件')
+
+		print response
+		return HttpResponse(response)
 def manageindex(request):
 	return HttpResponse('manageindex')
