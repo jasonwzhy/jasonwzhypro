5a8dbf6a3dc84ee942c2d3e270c581815efcc0d7
jasonwzhy <jasonwzhy@gmail.com>
2015-03-12 22:53:46 +0800

    up

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from blog import views
 urlpatterns = patterns('',
 	url(r'^$',views.index),
-	# url(r'^manage/'),
+	url(r'^manage/$',views.manage),
+	url(r'^manage/upload/$',views.upload),
 )
--- blog/views.py
+++ blog/views.py
@@ -3,17 +3,42 @@ from django.http import HttpResponse
 from django.template import RequestContext
 
 from blog.models import Article
+import os
+import json
 # Create your views here.
+
+def handle_uploaded_file(f):
+	print "in handle_uploaded_file"
+	fpath = os.getcwd()+'/blog/upload/name.jpg'
+	print fpath
+	with open(fpath,'wb+') as destination:
+		print f
+		for chunk in f.chunks():
+			destination.write(chunk)
 
 def index(request):
 	blogs = Article.objects.all()
 
 	return render_to_response("blog/blogpost.html",{"blogs":blogs},RequestContext(request))
 def blogitem(request):
-	
+	pass
 
 ###  Manage ####
 def manage(request):
 	
-	return render_to_response();
+	return render_to_response("blog/manage.html",RequestContext(request));
 
+def upload(request):
+	print request.method
+	if request.method == 'POST':
+		print request.FILES['upload_file']
+		
+		handle_uploaded_file(request.FILES['upload_file'])
+		ret = {
+			"success":"true",
+			"file_path":"/blog/upload/name.jpg"
+		}
+		print "upload success"
+		return HttpResponse(json.dumps(ret));
+	print 'error'
+	return HttpResponse("error")
